8c2b2ec2a010eaca1a2fb4ea3f3f2b4e08fd43eb
Daniel1871 <ishkinin.daniel@yandex.ru>
2024-08-23 16:02:44 +0300

    test(main.cpp): add assert & test functions

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,12 @@ enum class Status {
 
 template <class T1, class T2> 
 void assert(const T1& t1, const T2& t2, const std::string_view& t1_str, const std::string_view& t2_str, const std::string_view& file, const unsigned& line,const std::string_view& func, const std::string_view& hint="") { 
-    if (t1 != t2) { 
+    if(t1 != t2) { 
         std::cout << file << "(" << line << "): " << func << ": "; 
-        std::cout << "ASSERT(" << t1_str << ", " << t2_str << ") failed: "; 
-        std::cout << t1 << " != " << t2 << "."; 
+        std::cout << "ASSERT(" << t1_str << ", " << t2_str << ") failed:\n\n"; 
+        std::cout << t1_str << ": " << t1 << "\n" << t2_str << ": " << t2; 
         if (!hint.empty()) {
-            std::cout << " Hint: " << hint; 
+            std::cout << "\nHint: " << hint; 
         } 
         std::cout << std::endl;
     } 
@@ -21,38 +21,60 @@ void assert(const T1& t1, const T2& t2, const std::string_view& t1_str, const st
 #define ASSERT(a, b) assert(a, b, #a, #b, __FILE__, __LINE__, __FUNCTION__) 
 #define ASSERT_HINT(a, b, hint) assert(a, b, #a, #b, __FILE__, __LINE__, __FUNCTION__, hint) 
 
+void testParse(){
+	Json::Value jsonVec;
+    Json::Reader reader;
+    reader.parse("test.json", jsonVec);
+    
+    Json::Task t1(2025, 8, 11, 1, 38, "Implement a 'smart' calculator that treats every user error as a new calculation method.");
+    Json::Task t2(2025, 8, 19, 10, 6, "Create a program that randomly replaces all spaces in a text with emojis.");
+    Json::Task t3(2024, 3, 2, 0, 56, "Write a program that generates random conspiracy theories based on user input.");
+    Json::Value testJsonVec = {t1, t2, t3};
+    
+    ASSERT_HINT(jsonVec, testJsonVec, "Incorrect json parsing.");
+}
+
+void testSort(){    
+    Json::Task t1(2025, 8, 11, 1, 38, "Implement a 'smart' calculator that treats every user error as a new calculation method.");
+    Json::Task t2(2025, 8, 19, 10, 6, "Create a program that randomly replaces all spaces in a text with emojis.");
+    Json::Task t3(2024, 3, 2, 0, 56, "Write a program that generates random conspiracy theories based on user input.");
+    
+    Json::Value jsonVec = {t1, t2, t3};
+    sortTasks(jsonVec);
+    
+    Json::Value sortedJsonVec = {t3, t1, t2};
+    
+    ASSERT_HINT(jsonVec, sortedJsonVec, "Incorrect sort of Json::Value");
+}
+
+
+
 
 int main() {
     /*Json::Value jsonVec;
     Json::Reader reader;
 
-    reader.parse("tasks.json", jsonVec);
-    reader.printFile();
-    std::cout << "\n--------------------------------------------------------------------------------\nResult after reading json file:\n\n";
-    reader.print(jsonVec);
+    reader.parse("test.json", jsonVec);*/
+    // reader.printFile();
+    // std::cout << "\n--------------------------------------------------------------------------------\nResult after parsing json file:\n\n";
+    // reader.print(jsonVec);
     
-    std::cout << "\n--------------------------------------------------------------------------------\nSorted jsonVec:\n\n";
+    /*std::cout << "\n--------------------------------------------------------------------------------\nSorted jsonVec:\n\n";
     sortTasks(jsonVec);
     reader.print(jsonVec);*/
     
-    ASSERT_HINT(1,2, "ggg");
-    ASSERT(1,2.5);
+    /*Json::Task t1(2025, 8, 11, 1, 38, "Implement a 'smart' calculator that treats every user error as a new calculation method.");
+    Json::Task t2(2025, 8, 19, 10, 6, "Create a program that randomly replaces all spaces in a text with emojis.");
+    Json::Task t3(2024, 3, 2, 0, 56, "Write a program that generates random conspiracy theories based on user input.");
+    Json::Value testJsonVec = {t1, t2, t3};
+    ASSERT_HINT(jsonVec, testJsonVec, "Incorrect json parsing.");
     
-    Json::Task t1(2023, 1, 1, 1, 1, "gg");
-    Json::Task t2(2023, 1, 1, 1, 1, "ggg");
-    std::cout << std::endl << std::boolalpha << (t1 == t2);
+    sortTasks(jsonVec);
+    Json::Value sortedJsonVec = {t3, t1, t1};
+    ASSERT_HINT(jsonVec, sortedJsonVec, "Incorrect sort of Json::Value");*/
     
-    //Json::Value v1 = {t1, t2};
-    //Json::Value v2 = {t1, t2};
+    testParse();
+    testSort();
     
-    //std::cout << std::boolalpha << (v1 == v2);
-    
-    std::vector<Json::Task> v1 = {t1,t2}; std::vector<Json::Task> v2 = {t1,t2};
-    std::cout << std::boolalpha << (v1 == v2);
-    
-    
-    
-    
-        
     return 0;
 }
