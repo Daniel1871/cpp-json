fc1f007f15e2840948d8b13e16238f72e2be3bfa
Daniel1871 <ishkinin.daniel@yandex.ru>
2024-08-23 16:06:15 +0300

    test(main.cpp): add testReader()

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,8 @@ void assert(const T1& t1, const T2& t2, const std::string_view& t1_str, const st
 #define ASSERT(a, b) assert(a, b, #a, #b, __FILE__, __LINE__, __FUNCTION__) 
 #define ASSERT_HINT(a, b, hint) assert(a, b, #a, #b, __FILE__, __LINE__, __FUNCTION__, hint) 
 
-void testParse(){
-	Json::Value jsonVec;
+void testParse() {
+    Json::Value jsonVec;
     Json::Reader reader;
     reader.parse("test.json", jsonVec);
     
@@ -34,7 +34,7 @@ void testParse(){
     ASSERT_HINT(jsonVec, testJsonVec, "Incorrect json parsing.");
 }
 
-void testSort(){    
+void testSort() {
     Json::Task t1(2025, 8, 11, 1, 38, "Implement a 'smart' calculator that treats every user error as a new calculation method.");
     Json::Task t2(2025, 8, 19, 10, 6, "Create a program that randomly replaces all spaces in a text with emojis.");
     Json::Task t3(2024, 3, 2, 0, 56, "Write a program that generates random conspiracy theories based on user input.");
@@ -47,7 +47,10 @@ void testSort(){
     ASSERT_HINT(jsonVec, sortedJsonVec, "Incorrect sort of Json::Value");
 }
 
-
+void testReader() {
+    testParse();
+    testSort();
+}
 
 
 int main() {
@@ -73,8 +76,8 @@ int main() {
     Json::Value sortedJsonVec = {t3, t1, t1};
     ASSERT_HINT(jsonVec, sortedJsonVec, "Incorrect sort of Json::Value");*/
     
-    testParse();
-    testSort();
+    testReader();
+    
     
     return 0;
 }
