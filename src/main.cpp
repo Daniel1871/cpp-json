0b777600734a4b5e44b6f2b20b122f5ee972cae5
Daniel1871 <ishkinin.daniel@yandex.ru>
2024-08-23 12:56:44 +0300

    test(main.cpp): add assert function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
 #include "reader.h"
+enum class Status {
+    ACTUAL,
+    IRRELEVANT,
+    BANNED
+};
+
+template <class T1, class T2> 
+void assert(const T1& t1, const T2& t2, const std::string_view& t1_str, const std::string_view& t2_str, const std::string_view& file, const unsigned& line,const std::string_view& func, const std::string_view& hint="") { 
+    if (t1 != t2) { 
+        std::cout << file << "(" << line << "): " << func << ": "; 
+        std::cout << "ASSERT(" << t1_str << ", " << t2_str << ") failed: "; 
+        std::cout << t1 << " != " << t2 << "."; 
+        if (!hint.empty()) {
+            std::cout << " Hint: " << hint; 
+        } 
+        std::cout << std::endl;
+    } 
+} 
+ 
+#define ASSERT(a, b) assert(a, b, #a, #b, __FILE__, __LINE__, __FUNCTION__) 
+#define ASSERT_HINT(a, b, hint) assert(a, b, #a, #b, __FILE__, __LINE__, __FUNCTION__, hint) 
+
 
 int main() {
-    Json::Value jsonVec;
+    /*Json::Value jsonVec;
     Json::Reader reader;
 
     reader.parse("tasks.json", jsonVec);
@@ -11,7 +33,12 @@ int main() {
     
     std::cout << "\n--------------------------------------------------------------------------------\nSorted jsonVec:\n\n";
     sortTasks(jsonVec);
-    reader.print(jsonVec);
+    reader.print(jsonVec);*/
+    
+    ASSERT_HINT(1,2, "ggg");
+    ASSERT(1,2.5);
+    
+    
         
     return 0;
 }
